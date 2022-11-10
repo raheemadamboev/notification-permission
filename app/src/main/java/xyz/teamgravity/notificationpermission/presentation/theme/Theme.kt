b8b42e45eb00f7201dfec0116643c75fd88952fd
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-10 13:27:57 +0500

    build hello screen

--- app/src/main/java/xyz/teamgravity/notificationpermission/presentation/theme/Theme.kt
+++ app/src/main/java/xyz/teamgravity/notificationpermission/presentation/theme/Theme.kt
@@ -34,7 +34,7 @@ fun NotificationPermissionTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
